cde577135064dc4c8ab9cd95391d37bddf03f0c4
Ibet Hernandez <ibethernandez92@gmail.com>
2020-10-03 09:52:03 -0500

    flask fixes

---
+++ Mission_to_Mars/mars_app.py
@@ -0,0 +1,28 @@
+# Import dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Set up Mongo connection
+app.config['MONGO_URI']= 'mongo://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#Route to main page
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# Create route to Mars scraper
+@app.route('/scrape')
+def scraper():
+    mars= mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}. mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
